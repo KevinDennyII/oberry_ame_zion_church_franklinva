fb323b36f11199a528d198f3226f14e960ee739b
kevindennyii <kevin.dennyii@gmail.com>
2020-07-20 01:19:28 -0500

    removed TailWindCSS text shadow plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,15 +3,8 @@ module.exports = {
   purge: [
     './src/**/*.js',
   ],
-  theme: {textShadow: {
-      default: '0 2px 0 #000',
-      sm: '4px 3px 2px rgba(150, 150, 150, 0.53)',
-      md: '0 2px 2px #000',
-      h1: '0 0 3px #FF0000, 0 0 5px #0000FF',
-      xl: '0 0 3px rgba(0, 0, 0, .8), 0 0 5px rgba(0, 0, 0, .9)',
-      none: 'none',
-    }},
-  variants: {textShadow: ['responsive', 'hover'],},
+  theme: {},
+  variants: {},
   // https://github.com/tailwindcss/custom-forms
-  plugins: [require("@tailwindcss/custom-forms"), require('tailwindcss-textShadow')],
+  plugins: [require("@tailwindcss/custom-forms")],
 };
