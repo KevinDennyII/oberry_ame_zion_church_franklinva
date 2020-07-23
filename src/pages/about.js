612990d9de4f7e7bfff9fa923a95c8093156d497
kevindennyii <kevin.dennyii@gmail.com>
2020-07-23 01:55:05 -0500

    Updated layout along with about us, contact and home page

--- src/pages/about.js
+++ src/pages/about.js
@@ -13,22 +13,16 @@ function AboutPage() {
         title="About"
       />
 
-      <section className="flex flex-col items-center md:flex-row">
-        <div className="md:w-3/5 lg:w-2/5 md:mr-8 sm:text-md md:text-1xl lg:text-1xl">
-
+      <section className="flex flex-col items-center ">
+        <div className="mx-auto sm:text-1xl md:text-1xl lg:text-1xl xl:text-2xl">
           <div>Worship:{' '}
             Sundays at 11:00 AM EST<br/>
           </div>
           <div>Bible Study:{' '}
             Sundays at 10:00 AM EST<br/><br/>
           </div>
-
-          {/*<cite className="block mt-4 text-xs font-bold text-right uppercase">*/}
-          {/*  – Thomas Nagel*/}
-          {/*</cite>*/}
         </div>
-
-        <figure className="w-3/3 md:w-3/3">
+        <figure className="w-3/3 md:w-3/3 xl:w-5/5">
           <img alt="O'Berry A. M. E. Zion Church Committee" className="rounded shadow-md" src={oberryChurchCommittee} />
         </figure>
       </section>
