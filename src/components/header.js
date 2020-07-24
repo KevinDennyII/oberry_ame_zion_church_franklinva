21b5852ee7797c9c3c051b3fd78b0ed2168faa8a
kevindennyii <kevin.dennyii@gmail.com>
2020-07-24 02:34:52 -0500

    updated styling of navigation buttons

--- src/components/header.js
+++ src/components/header.js
@@ -62,13 +62,13 @@ function Header() {
               title: `Contact`,
             },
           ].map((link) => (
-            <Link
-              className="block mt-4 text-white no-underline md:inline-block md:mt-0 md:ml-6"
+              <button
+                  className="bg-purple-700 hover:bg-purple-900 text-white font-bold py-2 px-4 rounded"
               key={link.title}
-              to={link.route}
+              // to={link.route}
             >
-              {link.title}
-            </Link>
+                <a href={link.route}>{link.title}</a>
+            </button>
           ))}
         </nav>
       </div>
