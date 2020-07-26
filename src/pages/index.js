2a5fea14cd8681f2ba790574aa5d4bc55435e643
kevindennyii <kevin.dennyii@gmail.com>
2020-07-25 22:14:14 -0500

    updated home page with different home image

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-import oberryAmeZionChurchPic from "../images/oberry_ame_zion_church.jpg";
+// import oberryAmeZionChurchPic from "../images/oberry_ame_zion_church.jpg";
 
 function IndexPage() {
   return (
@@ -12,15 +12,23 @@ function IndexPage() {
         title="Home"
       />
 
-      <section className="text-center">
-        <img
-          alt="O'berry A. M. E. Zion Church of Franklin, Virginia"
-          className="rounded-lg mx-auto w-full h-80 p-1 "
-          src={oberryAmeZionChurchPic}
-        />
-        {/*<h2 className="inline-block text-teal-900 sm:text-sm md:text-2xl lg:text-2xl">*/}
-        {/*  {`Welcome to O'Berry A. M. E. Zion Church of Franklin, Virginia!`}*/}
-        {/*</h2>*/}
+      <section id="home" className="h-screen">
+        {/*<img*/}
+        {/*  alt="O'berry A. M. E. Zion Church of Franklin, Virginia"*/}
+        {/*  className="rounded-lg mx-auto w-full h-80 p-1 "*/}
+        {/*  src={oberryAmeZionChurchPic}*/}
+        {/*/>*/}
+        <div className="pt-16 sm:pt-56 md:pt-48 lg:pt-48 sm:text-2xl md:text-3xl lg:text-4xl text-center roboto font-black text-white">
+            {/*<svg id="curved" viewBox="0 0 500 500">*/}
+            {/*    <path className="curvedtext" id="curve" d="M73.2,148.6c4-6.1,65.5-96.8,178.6-95.6c111.3,1.2,170.8,90.3,175.1,97" />*/}
+            {/*    <text width="500">*/}
+            {/*        <textPath xlinkHref="#curve">*/}
+                        {`Welcome`}<br/>{`To`}<br/>{`O'Berry A. M. E. Zion Church!`}
+            {/*        </textPath>*/}
+            {/*    </text>*/}
+            {/*</svg>*/}
+        </div>
+
       </section>
     </Layout>
   );
