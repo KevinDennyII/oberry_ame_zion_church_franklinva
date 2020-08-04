66e951bd897b8fe4e72101805d6c5768e54071ba
kevindennyii <kevin.dennyii@gmail.com>
2020-08-04 16:10:17 -0500

    updated home page text over top of hero image

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,12 +13,11 @@ function IndexPage() {
 
       <section id="home" className="h-screen">
         <div className="text-shadow pt-32 sm:pt-56 md:pt-48 lg:pt-48 sm:text-2xl md:text-3xl lg:text-4xl text-center roboto font-black text-white">
-          {`Welcome`}
-          <br />
-          {`To`}
+          {`Welcome To`}
           <br />
           {`O'Berry A. M. E. Zion Church!`}
         </div>
+        <div className="bg-opacity-25 bg-gray-400 font-extrabold text-xs md:text-base text-center text-indigo-100 shadow-xl">{`A little church with a big ❤`}</div>
       </section>
     </Layout>
   );
