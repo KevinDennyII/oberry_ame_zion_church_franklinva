18eac916eedb270f4de144342642119ad52b3c94
Kevin Denny II <kevin@kronickdigitalmedia.com>
2024-01-24 23:04:26 -0600

    updated Copyright Year

--- src/components/layout.js
+++ src/components/layout.js
@@ -27,7 +27,7 @@ function Layout({ children }) {
             {` `}with ❤
             <br />
             {`Copyright `}&copy;
-            {` 2022 O'Berry A. M. E. Zion Church. All Rights Reserved.`}
+            {` 2024 O'Berry A. M. E. Zion Church. All Rights Reserved.`}
           </p>
         </nav>
       </footer>
